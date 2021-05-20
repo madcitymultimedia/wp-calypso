d656794330df685a7f90532e08bdb2608e7a9779
Payton Swick <payton@foolord.com>
2021-05-19 20:55:40 -0400

    Add isCompact to WPOrderReviewLineItems

--- client/my-sites/checkout/composite-checkout/components/wp-order-review-line-items.tsx
+++ client/my-sites/checkout/composite-checkout/components/wp-order-review-line-items.tsx
@@ -3,7 +3,6 @@
  */
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import styled from '@emotion/styled';
 import {
 	CheckoutModal,
 	FormStatus,
@@ -24,6 +23,7 @@ import {
 	getCouponLineItemFromCart,
 	getCreditsLineItemFromCart,
 	isWpComProductRenewal,
+	styled,
 } from '@automattic/wpcom-checkout';
 import {
 	isDomainRegistration,
@@ -64,10 +64,11 @@ import getPriceTierForUnits from 'calypso/my-sites/plans/jetpack-plans/get-price
 
 import deleteIcon from './delete-icon.svg';
 
-const WPOrderReviewList = styled.ul< { theme?: Theme } >`
-	border-top: 1px solid ${ ( props ) => props.theme.colors.borderColorLight };
+const WPOrderReviewList = styled.ul< { theme?: Theme; isCompact?: boolean } >`
+	border-color: ${ ( props ) => props.theme.colors.borderColorLight };
+	border-top-width: ${ ( props ) => ( props.isCompact ? '0' : '1px' ) };
 	box-sizing: border-box;
-	margin: 20px 36px 20px 0;
+	margin: ${ ( props ) => ( props.isCompact ? '10px 0' : '20px 36px 20px 0' ) };
 	padding: 0;
 
 	.rtl & {
@@ -306,6 +307,7 @@ export function WPNonProductLineItem( {
 export function WPOrderReviewLineItems( {
 	className,
 	isSummary,
+	isCompact,
 	removeProductFromCart,
 	removeCoupon,
 	getItemVariants,
@@ -314,6 +316,7 @@ export function WPOrderReviewLineItems( {
 }: {
 	className?: string;
 	isSummary?: boolean;
+	isCompact?: boolean;
 	removeProductFromCart?: RemoveProductFromCart;
 	removeCoupon: RemoveCouponFromCart;
 	getItemVariants?: ( productSlug: WPCOMProductSlug ) => WPCOMProductVariant[];
@@ -325,7 +328,10 @@ export function WPOrderReviewLineItems( {
 	const couponLineItem = getCouponLineItemFromCart( responseCart );
 
 	return (
-		<WPOrderReviewList className={ joinClasses( [ className, 'order-review-line-items' ] ) }>
+		<WPOrderReviewList
+			isCompact={ isCompact }
+			className={ joinClasses( [ className, 'order-review-line-items' ] ) }
+		>
 			{ responseCart.products.map( ( product ) => {
 				return (
 					<WPOrderReviewListItem key={ product.uuid }>
