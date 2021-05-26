502e9f59505a51b82791128b9e260855159296d4
David Szabo <david.szabo97@gmail.com>
2021-05-26 14:19:13 +0200

    Improve tracking

--- apps/wpcom-block-editor/src/wpcom/features/tracking/delegate-event-tracking.js
+++ apps/wpcom-block-editor/src/wpcom/features/tracking/delegate-event-tracking.js
@@ -13,7 +13,6 @@ import wpcomBlockDonationsPlanUpgrade from './wpcom-block-donations-plan-upgrade
 import wpcomBlockDonationsStripeConnect from './wpcom-block-donations-stripe-connect';
 import wpcomBlockPremiumContentPlanUpgrade from './wpcom-block-premium-content-plan-upgrade';
 import wpcomBlockPremiumContentStripeConnect from './wpcom-block-premium-content-stripe-connect';
-import wpcomBlockEditorCustomPostTemplateSave from './wpcom-block-editor-custom-post-template-save';
 import wpcomBlockEditorCustomPostTemplateActionsClick from './wpcom-block-editor-custom-post-template-actions-click';
 
 // Debugger.
@@ -50,7 +49,6 @@ const EVENTS_MAPPING = [
 	wpcomBlockDonationsStripeConnect(),
 	wpcomBlockPremiumContentPlanUpgrade(),
 	wpcomBlockPremiumContentStripeConnect(),
-	wpcomBlockEditorCustomPostTemplateSave(),
 	wpcomBlockEditorCustomPostTemplateActionsClick(),
 ];
 const EVENTS_MAPPING_CAPTURE = EVENTS_MAPPING.filter( ( { capture } ) => capture );
