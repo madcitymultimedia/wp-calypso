6e8c3b0cb88bc144aac95e40ba80d84e0058b189
Payton Swick <payton@foolord.com>
2021-06-11 14:19:17 -0400

    Remove absolute positioning on delete button

--- client/my-sites/checkout/composite-checkout/components/wp-order-review-line-items.tsx
+++ client/my-sites/checkout/composite-checkout/components/wp-order-review-line-items.tsx
@@ -159,11 +159,8 @@ const LineItemPriceWrapper = styled.span< { theme?: Theme; isSummary?: boolean }
 	}
 `;
 
-const DeleteButton = styled( Button )< { theme?: Theme; coupon?: boolean } >`
-	position: absolute;
-	padding: 10px;
-	right: -50px;
-	top: ${ ( props ) => ( props.coupon ? '0px' : '7px' ) };
+const DeleteButton = styled( Button )< { theme?: Theme } >`
+	padding: 0 0 0 10px;
 
 	:hover rect {
 		fill: ${ ( props ) => props.theme.colors.error };
@@ -171,11 +168,6 @@ const DeleteButton = styled( Button )< { theme?: Theme; coupon?: boolean } >`
 
 	svg {
 		opacity: 1;
-	}
-
-	.rtl & {
-		right: auto;
-		left: -50px;
 	}
 `;
 
@@ -224,7 +216,6 @@ function DeleteIcon( { uniqueID, product }: { uniqueID: string; product: string 
 
 export function WPNonProductLineItem( {
 	lineItem,
-	coupon,
 	className = null,
 	isSummary,
 	hasDeleteButton,
@@ -232,7 +223,6 @@ export function WPNonProductLineItem( {
 	createUserAndSiteBeforeTransaction,
 }: {
 	lineItem: LineItemType;
-	coupon?: boolean;
 	className?: string | null;
 	isSummary?: boolean;
 	hasDeleteButton?: boolean;
@@ -275,7 +265,6 @@ export function WPNonProductLineItem( {
 			{ hasDeleteButton && removeProductFromCart && formStatus === FormStatus.READY && (
 				<>
 					<DeleteButton
-						coupon={ coupon }
 						className="checkout-line-item__remove-product"
 						buttonType="borderless"
 						disabled={ isDisabled }
@@ -350,7 +339,6 @@ export function WPOrderReviewLineItems( {
 			{ couponLineItem && (
 				<WPOrderReviewListItem key={ couponLineItem.id }>
 					<NonProductLineItem
-						coupon
 						lineItem={ couponLineItem }
 						isSummary={ isSummary }
 						hasDeleteButton={ ! isSummary }
@@ -914,18 +902,6 @@ function WPLineItem( {
 					isSummary={ isSummary }
 				/>
 			</span>
-			{ sublabel && (
-				<LineItemMeta>
-					<LineItemSublabelAndPrice product={ product } />
-					<DomainDiscountCallout product={ product } />
-					<FirstTermDiscountCallout product={ product } />
-					<CouponDiscountCallout product={ product } />
-					<IntroductoryOfferCallout product={ product } />
-				</LineItemMeta>
-			) }
-			{ isJetpackSearch( product ) && <JetpackSearchMeta product={ product } /> }
-			{ isGSuite && <GSuiteUsersList product={ product } /> }
-			{ isTitanMail && <TitanMailMeta product={ product } isRenewal={ isRenewal } /> }
 			{ hasDeleteButton && removeProductFromCart && formStatus === FormStatus.READY && (
 				<>
 					<DeleteButton
@@ -970,6 +946,18 @@ function WPLineItem( {
 					/>
 				</>
 			) }
+			{ sublabel && (
+				<LineItemMeta>
+					<LineItemSublabelAndPrice product={ product } />
+					<DomainDiscountCallout product={ product } />
+					<FirstTermDiscountCallout product={ product } />
+					<CouponDiscountCallout product={ product } />
+					<IntroductoryOfferCallout product={ product } />
+				</LineItemMeta>
+			) }
+			{ isJetpackSearch( product ) && <JetpackSearchMeta product={ product } /> }
+			{ isGSuite && <GSuiteUsersList product={ product } /> }
+			{ isTitanMail && <TitanMailMeta product={ product } isRenewal={ isRenewal } /> }
 
 			{ shouldShowVariantSelector && getItemVariants && onChangePlanLength && (
 				<ItemVariationPicker
