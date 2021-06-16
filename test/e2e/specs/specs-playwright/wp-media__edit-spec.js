3a096445c74f114d1e9adddeec719f3e9df2a3e2
Edwin Takahashi <egao@outlook.com>
2021-06-15 22:29:18 -0700

    Add the same media edit spec for atomic.

--- test/e2e/specs/specs-playwright/wp-media__edit-spec.js
+++ test/e2e/specs/specs-playwright/wp-media__edit-spec.js
@@ -3,40 +3,80 @@
  */
 import { DataHelper, LoginFlow, MediaPage, SidebarComponent } from '@automattic/calypso-e2e';
 
-describe( DataHelper.createSuiteTitle( 'Media: Edit Media (Simple)' ), function () {
-	let mediaPage;
+describe( DataHelper.createSuiteTitle( 'Media: Edit Media' ), function () {
+	describe( 'Edit Image (Simple)', function () {
+		let mediaPage;
 
-	it( 'Log In', async function () {
-		const loginFlow = new LoginFlow( this.page );
-		await loginFlow.login();
+		it( 'Log In', async function () {
+			const loginFlow = new LoginFlow( this.page );
+			await loginFlow.login();
+		} );
+
+		it( 'Navigate to Media', async function () {
+			const sidebarComponent = await SidebarComponent.Expect( this.page );
+			await sidebarComponent.gotoMenu( { heading: 'Media' } );
+		} );
+
+		it( 'See media gallery', async function () {
+			mediaPage = await MediaPage.Expect( this.page );
+		} );
+
+		it( 'Show only images', async function () {
+			await mediaPage.clickTab( { name: 'Images' } );
+		} );
+
+		it( 'Select the first image item', async function () {
+			await mediaPage.clickOn( { item: 1 } );
+		} );
+
+		it( 'Click to edit selected image', async function () {
+			await mediaPage.editImage();
+		} );
+
+		it( 'Rotate image', async function () {
+			await mediaPage.rotateImage();
+		} );
+
+		it( 'Cancel image edit', async function () {
+			await mediaPage.cancelImageEdit();
+		} );
 	} );
 
-	it( 'Navigate to Media', async function () {
-		const sidebarComponent = await SidebarComponent.Expect( this.page );
-		await sidebarComponent.gotoMenu( { heading: 'Media' } );
-	} );
+	describe( 'Edit Image (Atomic)', function () {
+		let mediaPage;
 
-	it( 'See media gallery', async function () {
-		mediaPage = await MediaPage.Expect( this.page );
-	} );
+		it( 'Log In', async function () {
+			const loginFlow = new LoginFlow( this.page, 'wooCommerceUser' );
+			await loginFlow.login();
+		} );
 
-	it( 'Show only images', async function () {
-		await mediaPage.clickTab( { name: 'Images' } );
-	} );
+		it( 'Navigate to Media', async function () {
+			const sidebarComponent = await SidebarComponent.Expect( this.page );
+			await sidebarComponent.gotoMenu( { heading: 'Media' } );
+		} );
 
-	it( 'Select the first image item', async function () {
-		await mediaPage.clickOn( { item: 1 } );
-	} );
+		it( 'See media gallery', async function () {
+			mediaPage = await MediaPage.Expect( this.page );
+		} );
 
-	it( 'Click to edit selected image', async function () {
-		await mediaPage.editImage();
-	} );
+		it( 'Show only images', async function () {
+			await mediaPage.clickTab( { name: 'Images' } );
+		} );
 
-	it( 'Rotate image', async function () {
-		await mediaPage.rotateImage();
-	} );
+		it( 'Select the first image item', async function () {
+			await mediaPage.clickOn( { item: 1 } );
+		} );
 
-	it( 'Cancel image edit', async function () {
-		await mediaPage.cancelImageEdit();
+		it( 'Click to edit selected image', async function () {
+			await mediaPage.editImage();
+		} );
+
+		it( 'Rotate image', async function () {
+			await mediaPage.rotateImage();
+		} );
+
+		it( 'Cancel image edit', async function () {
+			await mediaPage.cancelImageEdit();
+		} );
 	} );
 } );
